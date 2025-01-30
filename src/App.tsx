4816592566a619893b9c3bf13896ca935f53e05b
Keanu Pahio <keanuph@gmail.com>
2025-01-30 13:42:59 -0800

    fix intro section height and init planning section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Intro from "./sections/Intro.js";
 import LandingBanner from "./sections/LandingBanner.js";
+import Planning from "./sections/Planning.js";
 function App() {
   return (
     <>
       <LandingBanner />
       <Intro />
+      <Planning />
     </>
   );
 }
---
+++ src/sections/Planning.tsx
@@ -0,0 +1,11 @@
+import "../styles/planning.css";
+
+const Planning = () => {
+  return (
+    <section>
+      <h1>Planning</h1>
+    </section>
+  );
+};
+
+export default Planning;
